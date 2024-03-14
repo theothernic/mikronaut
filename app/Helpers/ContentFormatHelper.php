81ffad56241c78ce7a28a5bc439024b54c6d03b0
Nic Barr <1134853+theothernic@users.noreply.github.com>
2024-03-14 00:01:15 -0500

    refactor: handle some existing content data idiosyncracies.

--- app/Helpers/ContentFormatHelper.php
+++ app/Helpers/ContentFormatHelper.php
@@ -14,7 +14,8 @@
         public static function formatHtml(string $content): string
         {
             // paragraph tags.
-            $content = self::formatLineBreaksAsHtml($content);
+            $content = self::formatLineBreaksAsParagraphs($content);
+            $content = self::removeStrayBreakTags($content);
             $content = self::formatRemoveEmptyParagraphs($content);
             // special HTML chars.
             $content = htmlspecialchars($content);
@@ -27,12 +28,25 @@
             return sprintf('<p>%s</p>', str_replace("<br /><br />", "</p><p>", $content));
         }
 
+        public static function removeStrayBreakTags(string $content): string
+        {
+            $repl = [
+                '<br />' => '',
+                '<br>' => ''
+            ];
+
+            foreach ($repl as $from => $to)
+                $content = str_replace($to, $from, $content);
+
+            return $content;
+        }
+
         public static    function formatRemoveEmptyParagraphs(string $content): string
         {
             return str_replace("<p></p>", '', $content);
         }
 
-        public static function formatLineBreaksAsHtml(string $content): string
+        public static function formatLineBreaksAsParagraphs(string $content): string
         {
             return sprintf('<p>%s</p>', str_replace("\r\n", "</p><p>", $content));
         }
